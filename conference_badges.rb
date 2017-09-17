9c076181136e11d384036b373de62dd3469df66e
Emily Morgan <emorgan05@gmail.com>
2017-09-17 22:07:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  attendees.each do |attendee|
+    puts batch_badge_creator(attendee)
+    puts assign_rooms(attendee)
+  end
 end
