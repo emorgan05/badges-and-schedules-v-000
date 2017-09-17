530d4bd90b2320ea9793d9aa48a53bd5207c0ce3
Emily Morgan <emorgan05@gmail.com>
2017-09-17 21:54:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+attendees = []
+def batch_badge_creator(names)
+  names.each do |name|
+    attendees.push(badge_maker(name))
+  end
+end
