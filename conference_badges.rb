d552b0f78c6aad070bfcef7ee4963bea6b890590
Emily Morgan <emorgan05@gmail.com>
2017-09-17 21:59:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 def assign_rooms(attendees)
   attendees.each_with_index do |attendee, index|
     number = index + 1
-    "Hello #{attendee}! You'll be assigned to room #{number}!"
+    attendees.push("Hello #{attendee}! You'll be assigned to room #{number}!")
   end
 end
